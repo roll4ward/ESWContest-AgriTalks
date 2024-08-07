128d947975048094af0c4ea6d0e3db21616c192f
Lee Junhee <junhee0110@gmail.com>
2024-08-08 00:32:05 +0900

    [Feat] Node.js https module로 변경
    - Fetch를 사용하는 것이 문제되는 것 같아서 변경
    - 일단은 오류없이 해결됨

--- webos/service/aitallk/service.js
+++ webos/service/aitallk/service.js
@@ -1,30 +1,64 @@
 var pkg_info = require("./package.json");
 let service_id = pkg_info.name;
 
+const http = require("https");
+
 var Service = require("webos-service");
 var aitalk_service = new Service(service_id);
 
 // ai와 talk 요청 method
-aitalk_service.register("talk", (message) => {
+aitalk_service.register("talk", (message) => { 
   var command = message.payload.command
     ? message.payload.command
     : "No send message...";
 
-  let url = "http://117.16.43.106:8000/ask";
+  let url = "https://rollforward.free.beeceptor.com/ask";
 
-  const result = fetch(url, {
+  const request = http.request(url, {
     method: "POST",
+    timeout: 2000,
     headers: {
       "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ prompt: { prompt: command } }),
-  })
-    .then((res) => res.json())
-    .then((json) => json.answer)
-    .catch((e) => "Error: " + e.message);
+    }},
+    (res)=> {
+      let data = "";
 
-  message.respond({
-    returnValue: true,
-    Response: result,
+      res.on("data", (chunk) => {
+        console.log("data arrived");
+        data += chunk;
+        });
+
+      res.on("end", () => {
+        console.log("end event");
+        try {
+          const json = JSON.parse(data);
+          console.log("parse json");
+          message.respond({
+            returnValue: true,
+            Response: json.answer,
+          });
+          console.log("message sent");
+        }
+        catch (err) {
+          console.log("error occured");
+          message.respond({
+            returnValue: false,
+            errorMessage: "Error occured while parsing JSON",
+          });
+          console.log("message sent")
+        }
+      })
+    });
+  request.on("timeout", ()=> {
+    console.log("timeout");
+    message.respond({
+      returnValue: false,
+      errorMessage: "Timeout",
+    });
+    console.log("message sent");
   });
+
+  console.log("before request");
+  request.end(JSON.stringify({ prompt: { prompt: command } }));
+  console.log("after request");
 });
