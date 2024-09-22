4a01bc9fbbcda1e696dae1f487fdd2c01ddf80ae
uush <dbtmd447@gmail.com>
2024-09-22 19:50:16 +0900

    [feat] 대시보드 페이지를 루트에 연결

--- webos/app/src/App.js
+++ webos/app/src/App.js
@@ -1,3 +1,4 @@
+import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -8,7 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/chat" element={<ChatPage />} />
         <Route path="/camera" element={<Camera />} />
       </Routes>
