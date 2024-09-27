c0eb8abd55ec9d15ddac414998182c17f546f2ab
_학생_컴퓨터과학과 김준호 <202331021@sangmyung.kr>
2024-09-27 14:47:50 +0900

    ask, voice_ask, stt, tts, ask_stream 순의 서비스들을 정리 및 구현함. ask_stream을 호출 할 때는 luna-send의 옵션 중 -i 를 붙여서 호출해야함.

--- webos/service/aitalk/service.js
+++ webos/service/aitalk/service.js
@@ -18,22 +18,22 @@ const aitalk_service = new Service(pkg_info.name);
 
 
 
-async function ask(msg) {
-  if (!("prompt" in msg.payload)) {
+// aitalk method
+aitalk_service.register("ask", async function(msg) { 
+  if (!("prompt" in msg.payload)) 
+  {
     msg.respond(new error('prompt is required.'));
     return;
   }
   const prompt = msg.payload.prompt;
 
-  if (thread == null) {
+  if (thread == null) 
+  {
     thread = await openai.beta.threads.create();    
   } 
   console.log("thread_id", thread.id);
 
-  const threadMessages = await openai.beta.threads.messages.create(
-      thread.id,
-      { role: "user", content: prompt }
-    );
+  const threadMessages = await openai.beta.threads.messages.create(thread.id, { role: "user", content: prompt });
 
   let run = await openai.beta.threads.runs.createAndPoll(
       thread.id,
@@ -43,28 +43,7 @@ async function ask(msg) {
       run.thread_id
   );
 
-  return messages.data[0].content[0].text.value
-}
-
-async function tts(msg) {
-  let speechFile = msg.store_path? msg.store_path: "/tmp/tts.mp3"
-  const mp3 = await openai.audio.speech.create({
-    model: "tts-1",
-    voice: "alloy",
-    input: msg.text,
-  });
-  console.log("mp3 file will be stored to " + speechFile);
-  const buffer = Buffer.from(await mp3.arrayBuffer());
-  await fs.promises.writeFile(speechFile, buffer);
-  console.log("TTS done.")
-}
-
-// aitalk method
-aitalk_service.register("ask", async function(msg) { 
-  const answer = await ask(msg)
-  console.log(answer)
-
-  msg.respond(new aitalk_response(answer));
+  msg.respond(new aitalk_response(messages.data[0].content[0].text.value))
 });
 
 aitalk_service.register("voice_ask", async function(msg) {
@@ -84,19 +63,46 @@ aitalk_service.register("voice_ask", async function(msg) {
   // 2. send voice file to API servce
   const transcription = await openai.audio.transcriptions.create({
       file: fs.createReadStream(msg.payload.voice_path),
-      model: "whisper-1",
+      model: config.openai_stt_model,
   });
 
   // 3. return text converted from voice file
   const voice_prompt = transcription.text;
 
-  const answer = await ask({payload: {prompt: voice_prompt}});
-  
-  msg.respond(new aitalk_response({
-    voice_prompt: voice_prompt,
-    answer: answer,
-  }));
-  return;
+  aitalk_service.call("luna://xyz.rollforward.app.aitalk/ask", {"prompt": voice_prompt}, async function(m2) {
+    console.log("This is ask call in voice call service")
+    msg.respond(new aitalk_response({
+      voice_prompt: voice_prompt,
+      answer: m2.payload.result    
+    }))
+  })
+});
+
+aitalk_service.register("stt", async function(msg) {
+  // 0. check msg contains "voice_path"
+  if (!("voice_path" in msg.payload)) {
+    msg.respond(new error('It requires voice_path (e.g. voice_text.mp3).'));
+    return;
+  }
+
+  // 1. check is voice file (e.g. mp3) is exist
+  // 1.1. if doesn't exist, then raise RuntimeError
+  if (!fs.existsSync(msg.payload.voice_path)) {
+    msg.respond(new error("File not found. ", msg.payload.voice_path))
+    return;
+  }
+
+  // 2. send voice file to API servce
+  const transcription = await openai.audio.transcriptions.create({
+      file: fs.createReadStream(msg.payload.voice_path),
+      model: config.openai_stt_model,
+  });
+
+  // 3. return text converted from voice file
+  const voice_prompt = transcription.text;
+  msg.respond(new aitalk_response(
+    {voice_prompt: voice_prompt}
+  ))
 });
 
 aitalk_service.register("tts", async function(msg) {
@@ -113,14 +119,62 @@ aitalk_service.register("tts", async function(msg) {
     return;
   }
 
-  await tts({
-    text: msg.payload.text, 
-    store_path: msg.payload.store_path
-  })
-  
+  const mp3 = await openai.audio.speech.create({
+    model: config.openai_tts.model,
+    voice: config.openai_tts.voice,
+    input: msg.payload.text,
+  });
+  console.log("mp3 file will be stored to " + msg.payload.store_path);
+  const buffer = Buffer.from(await mp3.arrayBuffer());
+  await fs.promises.writeFile(msg.payload.store_path, buffer);
+  console.log("TTS done.")
+
   msg.respond(new aitalk_response({
     store_path: msg.payload.store_path,
   }));
+});
 
-  return;
+aitalk_service.register("ask_stream", async function(msg) {
+  if (!("prompt" in msg.payload)) 
+  {
+    msg.respond(new error('prompt is required.'));
+    return;
+  }
+  const prompt = msg.payload.prompt;
+
+  if (thread == null) 
+  {
+    thread = await openai.beta.threads.create();    
+  } 
+  console.log("thread_id", thread.id);
+
+  const threadMessages = await openai.beta.threads.messages.create(thread.id, { role: "user", content: prompt });
+  await new Promise((resolve, reject) => {
+    try {
+      const stream = openai.beta.threads.runs.stream(thread.id, {
+        assistant_id: config.openai_assistant_id
+      })
+      .on('textCreated', (text) => {
+        console.log(text)
+      })
+      .on('textDelta', (textDelta, snapshot) => {
+        console.log(snapshot)
+        msg.respond(new aitalk_response({
+          chunks: snapshot,
+          is_streaming: true
+        }))
+      })
+      .on('end', () => {
+        console.log('\nStream has ended.');
+        resolve();
+        msg.respond(new aitalk_response({
+          chunks: "",
+          isStreaming: false
+        }))
+      })
+    } catch (err) {
+      console.error(err)
+    }
+  })
+  
 });
