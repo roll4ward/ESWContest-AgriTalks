72edf20199edba9c3508405c9d22f4bbbb001085
Lee Junhee <junhee0110@gmail.com>
2024-10-04 15:15:24 +0900

    [feat] 뒤로가기 버튼 추가

--- webos/app/src/component/SideBar.js
+++ webos/app/src/component/SideBar.js
@@ -1,13 +1,22 @@
 import styled from "styled-components";
+import arrowRight from "../assets/icon/arrowLeft.png";
+import { useNavigate } from "react-router-dom";
 
 export const SideBar = () => {
-  return <Container></Container>;
+  const navigate = useNavigate()
+  return (
+  <Container>
+    <img src={arrowRight} alt="" width={80} height={80} onClick={()=> {navigate(-1)}} />
+  </Container>);
 };
 
+const ButtonWrap = styled.div``;
 const Container = styled.div`
   display: flex;
+  flex-direction: column,
   width: 230px;
   height: 988px;
+  justify-content: center;
   background-color: #e4e2de;
   padding: 80px 0px;
   border-radius: 20px;
