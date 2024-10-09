6dc891a55191e7a5bf1d9eedd8c830867a094338
uush <dbtmd447@gmail.com>
2024-10-09 19:01:30 +0900

    [feat] 기기 추가 모달에 네트워크 연결 뷰 추가

--- webos/app/src/component/modal/RegisterDevice.js
+++ webos/app/src/component/modal/RegisterDevice.js
@@ -1,16 +1,16 @@
 import styled from "styled-components";
-import { ModalBase } from "./ModalBase"
-import { useRef, useState } from "react";
+import { ModalBase } from "./ModalBase";
+import { useState } from "react";
 import { SelectDevice } from "./registerDevice/SelectDevice";
 import { JoinNetwork } from "./registerDevice/JoinNetwork";
 import { InitializeDevice } from "./registerDevice/InitializeDevice";
 import { createToast } from "../../api/toast";
 
 const PAGE = {
-    SELECT_DEVICE : 0,
-    JOIN_NETWORK : 1,
-    INITIALIZE_DEVICE : 2
-}
+  SELECT_DEVICE: 0,
+  JOIN_NETWORK: 1,
+  INITIALIZE_DEVICE: 2,
+};
 
 export const RegisterDevice = () => {
     const [page, setPage] = useState(PAGE.SELECT_DEVICE);
@@ -53,43 +53,43 @@ export const RegisterDevice = () => {
 };
 
 const Container = styled.div`
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 0 20px;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 20px;
 `;
 
 const Title = styled.div`
-    margin-top: 15px;
-    text-align: center;
-    color: #448569;
-    font-size: 80px;
+  margin-top: 15px;
+  text-align: center;
+  color: #448569;
+  font-size: 80px;
 `;
 
 const ButtonWrap = styled.div`
-    margin-top: 25px;
-    margin-bottom: 15px;
-    width: 100%;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
+  margin-top: 25px;
+  margin-bottom: 15px;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 `;
 
 const Button = styled.button`
-    border-radius: 10px;
-    border: 0;
-    width: 48%;
-    height: 95px;
-    font-size: 50px;
-    text-align: center;
-    color: white;
-    background-color: ${props=>props.primary ? "#448569" : "#D9D9D9"};
+  border-radius: 10px;
+  border: 0;
+  width: 48%;
+  height: 95px;
+  font-size: 50px;
+  text-align: center;
+  color: white;
+  background-color: ${(props) => (props.primary ? "#448569" : "#D9D9D9")};
 `;
 
 const PageContainer = styled.div`
-    margin-top: 5px;
-    width: 100%;
-    flex-grow: 1;
+  margin-top: 5px;
+  width: 100%;
+  flex-grow: 1;
 `;
