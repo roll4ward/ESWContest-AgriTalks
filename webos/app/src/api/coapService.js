e453f532f11e7224266a3bc6f3e4676cdfad0fea
Lee Junhee <junhee0110@gmail.com>
2024-10-15 00:28:32 +0900

    [feat] read/recent 메소드를 호출하여 그래프 데이터 추출
    - 기존 앱에서 필터링하던 그래프 데이터를 서비스에서 처리해서
      줄 수 있도록 수정

--- webos/app/src/api/coapService.js
+++ webos/app/src/api/coapService.js
@@ -40,6 +40,33 @@ export function readAllValues(deviceID, callback) {
 }
 
 /**
+ * 해당 기기의 최근 몇시간의 데이터를 모두 불러옴
+ * @param {string} deviceID 값을 조회할 기기의 ID
+ * @param {number} hour 조회할 시간
+ * @param {*} callback 쿼리한 결과를 처리할 콜백 함수
+ */
+export function readRecentValues(deviceID, hour, callback) {
+    let bridge = new WebOSServiceBridge();
+    bridge.onservicecallback = (msg) => {
+        msg = JSON.parse(msg);
+        if(!msg.returnValue) {
+            console.log(`Service call failed : ${msg.results}`);
+            return;
+        }
+
+        if(callback) callback(msg.results);
+        console.log("Callback called ", callback);
+    }
+
+    let query = {
+        deviceId: deviceID,
+        hour: hour
+    };
+
+    bridge.call(getServiceURL("coap", "read/recent"), JSON.stringify(query));
+}
+
+/**
  * 해당 기기의 가장 최근 측정값을 읽어옴
  * @param {*} deviceID 
  * @param {*} callback 
